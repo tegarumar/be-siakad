9605644de18a1034f3123fc3edc8571741703d77
tegarumar29 <tegar.umar@gmail.com>
2025-05-19 11:06:42 +0700

    faet: enhance matkul controller and service (CRUD) with proper error exceptions handling and validation

--- src/mata-kuliah/mata-kuliah.controller.ts
+++ src/mata-kuliah/mata-kuliah.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  NotFoundException,
+  Put,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { MataKuliahService } from './mata-kuliah.service';
 import { CreateMataKuliahDto } from './dto/create-mata-kuliah.dto';
 import { UpdateMataKuliahDto } from './dto/update-mata-kuliah.dto';
@@ -6,29 +16,61 @@ import { UpdateMataKuliahDto } from './dto/update-mata-kuliah.dto';
 @Controller('mata-kuliah')
 export class MataKuliahController {
   constructor(private readonly mataKuliahService: MataKuliahService) {}
-
-  @Post()
-  create(@Body() createMataKuliahDto: CreateMataKuliahDto) {
-    return this.mataKuliahService.create(createMataKuliahDto);
-  }
-
   @Get()
   findAll() {
     return this.mataKuliahService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mataKuliahService.findOne(+id);
+  @Get(':kode')
+  findOne(@Param('kode') kode: string) {
+    const matkul = this.mataKuliahService.findOne(kode);
+
+    if (!matkul) {
+      throw new NotFoundException(
+        `Mata kuliah dengan kode ${kode} tidak ditemukan`,
+      );
+    }
+
+    return matkul;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMataKuliahDto: UpdateMataKuliahDto) {
-    return this.mataKuliahService.update(+id, updateMataKuliahDto);
+  @Post()
+  create(@Body() createMataKuliahDto: CreateMataKuliahDto) {
+    try {
+      return this.mataKuliahService.create(createMataKuliahDto);
+    } catch (error) {
+      if (error.getStatus) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Terjadi kesalahan tak dikenal');
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mataKuliahService.remove(+id);
+  @Put(':kode')
+  update(
+    @Param('kode') kode: string,
+    @Body() updateMataKuliahDto: UpdateMataKuliahDto,
+  ) {
+    try {
+      const updated = this.mataKuliahService.update(kode, updateMataKuliahDto);
+      return updated;
+    } catch (error) {
+      if (error.getStatus) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Terjadi kesalahan tak dikenal');
+    }
+  }
+
+  @Delete(':kode')
+  remove(@Param('kode') kode: string) {
+    try {
+      return this.mataKuliahService.remove(kode);
+    } catch (error) {
+      if (error.getStatus) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Terjadi kesalahan tak dikenal');
+    }
   }
 }
--- src/mata-kuliah/mata-kuliah.service.ts
+++ src/mata-kuliah/mata-kuliah.service.ts
@@ -1,26 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMataKuliahDto } from './dto/create-mata-kuliah.dto';
 import { UpdateMataKuliahDto } from './dto/update-mata-kuliah.dto';
+import { MataKuliah } from './entities/mata-kuliah.entity';
 
 @Injectable()
 export class MataKuliahService {
+  private data: MataKuliah[] = [];
   create(createMataKuliahDto: CreateMataKuliahDto) {
-    return 'This action adds a new mataKuliah';
+    if (
+      !createMataKuliahDto.kode ||
+      !createMataKuliahDto.nama ||
+      !createMataKuliahDto.sks ||
+      !createMataKuliahDto.semester ||
+      !createMataKuliahDto.jurusan
+    ) {
+      throw new BadRequestException('Semua field harus diisi');
+    }
+
+    // cek kalo kode udah ada
+    const existingMataKuliah = this.data.find(
+      (mataKuliah) => mataKuliah.kode === createMataKuliahDto.kode,
+    );
+
+    if (existingMataKuliah) {
+      // kalo dah ada, error karena kode harus unik
+      throw new ConflictException('Kode mata kuliah sudah ada');
+    }
+
+    const newMataKuliah = new MataKuliah(
+      createMataKuliahDto.kode,
+      createMataKuliahDto.nama,
+      createMataKuliahDto.sks,
+      createMataKuliahDto.semester,
+      createMataKuliahDto.jurusan,
+    );
+
+    this.data.push(newMataKuliah);
+    return newMataKuliah;
   }
 
   findAll() {
-    return `This action returns all mataKuliah`;
+    return this.data;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} mataKuliah`;
+  findOne(kode: string) {
+    return this.data.find((mataKuliah) => mataKuliah.kode === kode);
   }
 
-  update(id: number, updateMataKuliahDto: UpdateMataKuliahDto) {
-    return `This action updates a #${id} mataKuliah`;
+  update(
+    kode: string,
+    updateMataKuliahDto: UpdateMataKuliahDto,
+  ): MataKuliah | null {
+    if (
+      !updateMataKuliahDto.kode ||
+      !updateMataKuliahDto.nama ||
+      !updateMataKuliahDto.sks ||
+      !updateMataKuliahDto.semester ||
+      !updateMataKuliahDto.jurusan
+    ) {
+      throw new BadRequestException('Semua field harus diisi');
+    }
+
+    const index = this.data.findIndex((mataKuliah) => mataKuliah.kode === kode);
+    if (index === -1) {
+      throw new NotFoundException(
+        `Mata kuliah dengan kode ${kode} tidak ditemukan`,
+      );
+    }
+
+    const updatedMataKuliah = new MataKuliah(
+      updateMataKuliahDto.kode,
+      updateMataKuliahDto.nama,
+      updateMataKuliahDto.sks,
+      updateMataKuliahDto.semester,
+      updateMataKuliahDto.jurusan,
+    );
+    this.data[index] = updatedMataKuliah;
+    return updatedMataKuliah;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} mataKuliah`;
+  remove(kode: string): MataKuliah | null {
+    const index = this.data.findIndex((mataKuliah) => mataKuliah.kode === kode);
+    if (index === -1) {
+      throw new NotFoundException(
+        `Mata kuliah dengan kode ${kode} tidak ditemukan`,
+      );
+    }
+
+    const deletedMataKuliah = this.data[index];
+    this.data.splice(index, 1);
+    return deletedMataKuliah;
   }
 }
